82f300680c4679db9525c04314141898f7818236
Marcelo Marques <marcelomarqueslima.dev@outlook.com>
2024-03-18 13:07:56 -0300

    Add project files.

---
+++ AdvancedRestApi/Interfaces/IUser.cs
@@ -0,0 +1,13 @@
+﻿using AdvancedRestApi.Models;
+
+namespace AdvancedRestApi.Interfaces
+{
+    public interface IUser
+    {
+        Task<List<User>> GetAll();
+        Task<User> GetById(Guid id);
+        Task AddUser(User user);
+        Task UpdateUser(Guid id, User user);
+        Task DeleteUser(Guid id);
+    }
+}
